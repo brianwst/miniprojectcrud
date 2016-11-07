383339502d95c66d399703bacce702de022e7f02
Brian Wong <brian.wongst@gmail.com>
2016-11-07 18:14:49 +0800

    Add Edit, remove function, include form partial

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,15 +11,40 @@ class SongsController < ApplicationController
 	def new
 	end
 
+	def edit 
+		@song = Song.find(params[:id])
+	end
+
 	def create
 		@song = Song.new(params_screen)
 
-		@song.save
-		redirect_to @song
+		if @song.save
+			redirect_to song_path(@song)
+		else 
+			render 'new'
+		end
 	end
+
+	def update
+		@song = Song.find(params[:id])
+
+		if @song.update(params_screen)
+			redirect_to song_path(@song)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@song = Song.find(params[:id])
+		@song.destroy
+
+		redirect_to songs_path
+	end
+
 
 	private
 	def params_screen
-		params.require(:song).permit(:name, :singer)
+		params.require(:song).permit(:name, :singer, :lyrics)
 	end
 end
